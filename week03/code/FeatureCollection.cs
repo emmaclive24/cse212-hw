cc7118764dddc645b389f774ef8a601a078825d4
Clive Tumbare <131152728+emmaclive24@users.noreply.github.com>
2024-10-11 12:10:07 +0200

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -2,9 +2,9 @@ public class FeatureCollection
 {
 	// TODO Problem 5 - ADD YOUR CODE HERE
 	// Create additional classes as necessary
-	{
+	
 	public List<Feature> Features { get; set; }
-	}
+	
 }
 
 public class Feature
